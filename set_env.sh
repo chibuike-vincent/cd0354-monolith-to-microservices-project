0ba0b3995f654a3587b1438154776c2a0f4de5c3
chibuike-vincent <chibuikepatrick2@gmail.com>
2022-09-14 22:28:26 +0100

    pushing to pipeline

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export POSTGRES_USERNAME=vincent
+export POSTGRES_PASSWORD=vincentpassword
+export POSTGRES_HOST=udagramdb.czmemsqj7dic.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagramdb
+export AWS_BUCKET=thirdprojectbucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=testing
+export JWT_SECRET=mysecrettesting
 export URL=http://localhost:8100
